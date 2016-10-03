7f23cfb062d4e2db1e49418c824a9b2d5e3a7207
Cèsar Ordiñana <cordin@disid.com>
2016-10-03 16:02:45 +0200

    Remove entity formatters, they are no longer needed

--- spring-mvc-restful/src/main/java/com/disid/restful/web/ProductDeserializer.java
+++ spring-mvc-restful/src/main/java/com/disid/restful/web/ProductDeserializer.java
@@ -4,6 +4,7 @@
 package com.disid.restful.web;
 
 import com.disid.restful.model.Product;
+import com.disid.restful.service.api.ProductService;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.ObjectCodec;
@@ -13,38 +14,47 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 /**
+ * Deserializer which gets a {@link Product} id and returns a {@link Product} 
+ * instance loaded through the {@link ProductService}.
+ * The {@link ConversionService} is used to convert the {@link Product} id 
+ * from text to {@link Long}.
  * @author Cèsar Ordiñana
  */
 @Component
 public class ProductDeserializer extends JsonDeserializer<Product> {
 
-  private ProductFormatter formatter;
+  private ProductService productService;
+  private ConversionService conversionService;
 
   @Autowired
-  public ProductDeserializer(ProductFormatter formatter) {
-    this.formatter = formatter;
+  public ProductDeserializer(ProductService productService, ConversionService conversionService) {
+    this.productService = productService;
+    this.conversionService = conversionService;
   }
 
   @Override
   public Product deserialize(JsonParser jp, DeserializationContext ctxt)
       throws IOException, JsonProcessingException {
-    String id = null;
+    String idText = null;
     try {
       ObjectCodec codec = jp.getCodec();
       JsonNode tree = codec.readTree(jp);
-      id = tree.asText();
-      return formatter.parse(id, null);
+      idText = tree.asText();
+      Long id = conversionService.convert(idText, Long.class);
+      return productService.findOne(id);
     } catch (Exception ex) {
       if (ex instanceof IOException) {
         throw (IOException) ex;
       }
       String msg =
-          id == null ? "Product deserialize error" : "Could not find Product with id: " + id;
+          idText == null ? "Product deserialize error"
+              : "Could not find Product with id: " + idText;
       throw new JsonMappingException(jp, msg, ex);
     }
   }
