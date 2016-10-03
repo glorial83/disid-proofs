7425c32a853f87d0e8b8238506f713a891c060bd
Cèsar Ordiñana <cordin@disid.com>
2016-10-03 16:07:42 +0200

    Remove generics warnings and use ResponseEntity build methods

--- spring-mvc-restful/src/main/java/com/disid/restful/http/converter/json/ExceptionHandlerAdvice.java
+++ spring-mvc-restful/src/main/java/com/disid/restful/http/converter/json/ExceptionHandlerAdvice.java
@@ -1,6 +1,5 @@
 package com.disid.restful.http.converter.json;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.roo.addon.web.mvc.controller.annotations.http.converters.json.RooJSONExceptionHandlerAdvice;
 import org.springframework.validation.BindingResult;
@@ -16,9 +15,8 @@ public class ExceptionHandlerAdvice {
 
   @ExceptionHandler(BindingErrorException.class)
   @ResponseBody
-  @SuppressWarnings({"rawtypes", "unchecked"})
-  public ResponseEntity handleException(HttpServletRequest req, BindingErrorException ex) {
+  public ResponseEntity<?> handleException(HttpServletRequest req, BindingErrorException ex) {
     BindingResult result = ex.getBindingResult();
-    return new ResponseEntity(result, HttpStatus.UNPROCESSABLE_ENTITY);
+    return ResponseEntity.unprocessableEntity().body(result);
   }
 }
