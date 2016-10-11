17ecdb65c45252962a599534c563ce188ca76265
Cèsar Ordiñana <cordin@disid.com>
2016-10-11 18:24:58 +0200

    Use the new springlets-boot-starter-web to autoconfigure the springlets data web utilities

--- spring-mvc-restful/src/main/java/com/disid/restful/config/WebMvcConfiguration.java
+++ spring-mvc-restful/src/main/java/com/disid/restful/config/WebMvcConfiguration.java
@@ -1,32 +1,20 @@
 package com.disid.restful.config;
 
-import io.springlets.data.web.GlobalSearchHandlerMethodArgumentResolver;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import java.util.List;
 import java.util.Locale;
 
 @Configuration
+//@EnableSpringletsDataWebSupport
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
-
-  @Bean
-  public GlobalSearchHandlerMethodArgumentResolver globalSearchResolver() {
-    return new GlobalSearchHandlerMethodArgumentResolver();
-  }
-
-  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-    argumentResolvers.add(globalSearchResolver());
-  }
 
   @Primary
   @Bean
