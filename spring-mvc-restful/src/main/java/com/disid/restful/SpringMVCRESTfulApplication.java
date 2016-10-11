6ff2016253c51322e4385cdfa40609f89b7e8f80
Cèsar Ordiñana <cordin@disid.com>
2016-10-11 13:02:02 +0200

    Remove deactivation of Spring Data web autoconfiguration

--- spring-mvc-restful/src/main/java/com/disid/restful/SpringMVCRESTfulApplication.java
+++ spring-mvc-restful/src/main/java/com/disid/restful/SpringMVCRESTfulApplication.java
@@ -2,9 +2,8 @@ package com.disid.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
 
-@SpringBootApplication(exclude = SpringDataWebAutoConfiguration.class)
+@SpringBootApplication
 public class SpringMVCRESTfulApplication {
 
   public static void main(String[] args) {
--- spring-mvc-restful/src/main/java/com/disid/restful/config/WebMvcConfiguration.java
+++ spring-mvc-restful/src/main/java/com/disid/restful/config/WebMvcConfiguration.java
@@ -5,14 +5,11 @@ import io.springlets.data.web.GlobalSearchHandlerMethodArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.geo.format.DistanceFormatter;
-import org.springframework.data.geo.format.PointFormatter;
-import org.springframework.data.web.config.SpringDataWebConfiguration;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -20,7 +17,7 @@ import java.util.List;
 import java.util.Locale;
 
 @Configuration
-public class WebMvcConfiguration extends SpringDataWebConfiguration {
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
   @Bean
   public GlobalSearchHandlerMethodArgumentResolver globalSearchResolver() {
@@ -28,18 +25,7 @@ public class WebMvcConfiguration extends SpringDataWebConfiguration {
   }
 
   public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-    super.addArgumentResolvers(argumentResolvers);
     argumentResolvers.add(globalSearchResolver());
-  }
-
-  /**
-   * Overwrite SpringDataWebConfiguration behavior, as we don't want the
-   * registration of the DomainClassConverter.
-   */
-  @Override
-  public void addFormatters(FormatterRegistry registry) {
-    registry.addFormatter(DistanceFormatter.INSTANCE);
-    registry.addFormatter(PointFormatter.INSTANCE);
   }
 
   @Primary
