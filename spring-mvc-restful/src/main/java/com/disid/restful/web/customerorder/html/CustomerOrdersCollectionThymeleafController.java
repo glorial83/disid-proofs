a56ae89faf740d8b7ccf717088768633e1501e99
jmvivo <jmvivoa1@gmail.com>
2016-10-28 08:59:21 +0200

    Organized imports

--- spring-mvc-restful/src/main/java/com/disid/restful/web/customerorder/html/CustomerOrdersCollectionThymeleafController.java
+++ spring-mvc-restful/src/main/java/com/disid/restful/web/customerorder/html/CustomerOrdersCollectionThymeleafController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriComponents;
 
 import javax.validation.Valid;
--- spring-mvc-restful/src/main/java/com/disid/restful/web/product/html/ProductsItemThymeleafController.java
+++ spring-mvc-restful/src/main/java/com/disid/restful/web/product/html/ProductsItemThymeleafController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriComponents;
 
 import java.util.Locale;
