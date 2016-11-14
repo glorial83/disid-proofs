ebd79b655148d32b30072b29f13c61a3b75b9df8
Cèsar Ordiñana <cordin@disid.com>
2016-11-14 09:05:33 +0100

    Avoid receiving the id value

--- spring-mvc-restful/src/main/java/com/disid/restful/web/category/html/CategoriesItemThymeleafController.java
+++ spring-mvc-restful/src/main/java/com/disid/restful/web/category/html/CategoriesItemThymeleafController.java
@@ -12,8 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -40,6 +42,11 @@ public class CategoriesItemThymeleafController {
       MessageSource messageSource) {
     this.categoryService = categoryService;
     this.messageSource = messageSource;
+  }
+
+  @InitBinder("category")
+  public void initCustomerBinder(WebDataBinder dataBinder) {
+    dataBinder.setDisallowedFields("id");
   }
 
   @ModelAttribute
