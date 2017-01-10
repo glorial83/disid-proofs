feeb9af1f0dbe2e62b16056053afbc79fcc2e717
jcagarcia <jugade92@gmail.com>
2017-01-10 13:29:11 +0100

    First commit of a proof of concept to analyze and test how to integrate a detail list into the parent show view

---
+++ spring-roo-show-view-with-detail/src/main/java/org/springframework/roo/petclinic/domain/Visit.java
@@ -0,0 +1,79 @@
+package org.springframework.roo.petclinic.domain;
+import org.springframework.roo.addon.javabean.annotations.RooJavaBean;
+import org.springframework.roo.addon.javabean.annotations.RooToString;
+import org.springframework.roo.addon.jpa.annotations.entity.RooJpaEntity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Version;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import org.springframework.format.annotation.DateTimeFormat;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import org.springframework.roo.addon.ws.annotations.jaxb.RooJaxbEntity;
+
+/**
+ * = Visit
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@RooJavaBean
+@RooToString
+@RooJpaEntity
+@RooJaxbEntity
+public class Visit {
+
+    /**
+     * TODO Auto-generated field documentation
+     *
+     */
+    @Id
+    @SequenceGenerator(name = "visitGen", sequenceName = "VISIT_SEQ")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "visitGen")
+    private Long id;
+
+    /**
+     * TODO Auto-generated field documentation
+     *
+     */
+    @Version
+    private Integer version;
+
+    /**
+     * TODO Auto-generated field documentation
+     *
+     */
+    @Size(max = 255)
+    private String description;
+
+    /**
+     * TODO Auto-generated field documentation
+     *
+     */
+    @NotNull
+    @Past
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date visitDate;
+
+    /**
+     * TODO Auto-generated field documentation
+     *
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Pet pet;
+
+    /**
+     * TODO Auto-generated field documentation
+     *
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Vet vet;
+}
